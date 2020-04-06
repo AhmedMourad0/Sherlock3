dd782f90a324fa40dbc3948871eaa726bbba8f74
Ahmed Mourad <dev.ahmedmourad73744@gmail.com>
2020-04-06 15:13:41 +0200

    Deleted DeeplyLinkedController.kt

--- app/src/main/java/inc/ahmedmourad/sherlock/model/common/DeeplyLinkedController.kt
+++
@@ -1,9 +0,0 @@
-package inc.ahmedmourad.sherlock.model.common
-
-import android.content.Intent
-import com.bluelinelabs.conductor.Router
-
-internal interface DeeplyLinkedController {
-    fun isDestination(destinationId: Int): Boolean
-    fun navigate(router: Router, intent: Intent)
-}
