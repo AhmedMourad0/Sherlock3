54facf126df29e17607b612d263992c6e527f630
Ahmed Mourad <dev.ahmedmourad73744@gmail.com>
2020-04-06 15:23:40 +0200

    Removed unnecessary interactors that are now part of the GlobalViewModel

--- app/src/main/java/inc/ahmedmourad/sherlock/viewmodel/activity/MainActivityViewModel.kt
+++ app/src/main/java/inc/ahmedmourad/sherlock/viewmodel/activity/MainActivityViewModel.kt
@@ -1,39 +1,9 @@
 package inc.ahmedmourad.sherlock.viewmodel.activity
 
 import androidx.lifecycle.ViewModel
-import arrow.core.Either
-import inc.ahmedmourad.sherlock.domain.interactors.auth.FindSignedInUserInteractor
-import inc.ahmedmourad.sherlock.domain.interactors.auth.ObserveUserAuthStateInteractor
 import inc.ahmedmourad.sherlock.domain.interactors.auth.SignOutInteractor
-import inc.ahmedmourad.sherlock.domain.interactors.common.ObserveInternetConnectivityInteractor
-import inc.ahmedmourad.sherlock.domain.model.auth.IncompleteUser
-import inc.ahmedmourad.sherlock.domain.model.auth.SignedInUser
-import inc.ahmedmourad.sherlock.model.common.Connectivity
-import io.reactivex.Flowable
 import io.reactivex.android.schedulers.AndroidSchedulers
 
-internal class MainActivityViewModel(
-        observeInternetConnectivityInteractor: ObserveInternetConnectivityInteractor,
-        observeUserAuthStateInteractor: ObserveUserAuthStateInteractor,
-        findSignedInUserInteractor: FindSignedInUserInteractor,
-        signOutInteractor: SignOutInteractor
-) : ViewModel() {
-
-    val internetConnectivityFlowable: Flowable<Connectivity> = observeInternetConnectivityInteractor()
-            .map(this::getConnectivity)
-            .retry()
-            .observeOn(AndroidSchedulers.mainThread())
-
-    val isUserSignedInSingle: Flowable<Boolean> = observeUserAuthStateInteractor()
-            .observeOn(AndroidSchedulers.mainThread())
-
-    val findSignedInUserSingle: Flowable<Either<Throwable, Either<IncompleteUser, SignedInUser>>> =
-            findSignedInUserInteractor().observeOn(AndroidSchedulers.mainThread())
-
-    val signOutSingle = signOutInteractor()
-            .observeOn(AndroidSchedulers.mainThread())
-
-    private fun getConnectivity(isConnected: Boolean): Connectivity {
-        return if (isConnected) Connectivity.CONNECTED else Connectivity.DISCONNECTED
-    }
+internal class MainActivityViewModel(signOutInteractor: SignOutInteractor) : ViewModel() {
+    val signOutSingle = signOutInteractor().observeOn(AndroidSchedulers.mainThread())
 }
--- app/src/main/java/inc/ahmedmourad/sherlock/viewmodel/activity/factory/MainActivityViewModelFactory.kt
+++ app/src/main/java/inc/ahmedmourad/sherlock/viewmodel/activity/factory/MainActivityViewModelFactory.kt
@@ -2,23 +2,14 @@ package inc.ahmedmourad.sherlock.viewmodel.activity.factory
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
-import inc.ahmedmourad.sherlock.domain.interactors.auth.FindSignedInUserInteractor
-import inc.ahmedmourad.sherlock.domain.interactors.auth.ObserveUserAuthStateInteractor
 import inc.ahmedmourad.sherlock.domain.interactors.auth.SignOutInteractor
-import inc.ahmedmourad.sherlock.domain.interactors.common.ObserveInternetConnectivityInteractor
 import inc.ahmedmourad.sherlock.viewmodel.activity.MainActivityViewModel
 
 internal class MainActivityViewModelFactory(
-        private val observeInternetConnectivityInteractor: ObserveInternetConnectivityInteractor,
-        private val observeUserAuthStateInteractor: ObserveUserAuthStateInteractor,
-        private val findSignedInUserInteractor: FindSignedInUserInteractor,
         private val signOutInteractor: SignOutInteractor
 ) : ViewModelProvider.NewInstanceFactory() {
     @Suppress("UNCHECKED_CAST")
     override fun <T : ViewModel> create(modelClass: Class<T>) = MainActivityViewModel(
-            observeInternetConnectivityInteractor,
-            observeUserAuthStateInteractor,
-            findSignedInUserInteractor,
             signOutInteractor
     ) as T
 }
