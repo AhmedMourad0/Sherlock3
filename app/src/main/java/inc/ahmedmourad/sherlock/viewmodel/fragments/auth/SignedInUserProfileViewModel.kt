ffc82ad3f9e0415833b16d885810905d040ae0ee
Ahmed Mourad <dev.ahmedmourad73744@gmail.com>
2020-04-06 15:05:13 +0200

    Renamed FindSignedInUserInteractor to ObserveSignedInUserInteractor

--- app/src/main/java/inc/ahmedmourad/sherlock/viewmodel/fragments/auth/SignedInUserProfileViewModel.kt
+++ app/src/main/java/inc/ahmedmourad/sherlock/viewmodel/fragments/auth/SignedInUserProfileViewModel.kt
@@ -2,13 +2,13 @@ package inc.ahmedmourad.sherlock.viewmodel.fragments.auth
 
 import androidx.lifecycle.ViewModel
 import arrow.core.Either
-import inc.ahmedmourad.sherlock.domain.interactors.auth.FindSignedInUserInteractor
+import inc.ahmedmourad.sherlock.domain.interactors.auth.ObserveSignedInUserInteractor
 import inc.ahmedmourad.sherlock.domain.model.auth.IncompleteUser
 import inc.ahmedmourad.sherlock.domain.model.auth.SignedInUser
 import io.reactivex.Flowable
 import io.reactivex.android.schedulers.AndroidSchedulers
 
-internal class SignedInUserProfileViewModel(interactor: FindSignedInUserInteractor) : ViewModel() {
+internal class SignedInUserProfileViewModel(interactor: ObserveSignedInUserInteractor) : ViewModel() {
     val signedInUserSingle: Flowable<Either<Throwable, Either<IncompleteUser, SignedInUser>>> =
             interactor().observeOn(AndroidSchedulers.mainThread())
 }
--- app/src/main/java/inc/ahmedmourad/sherlock/viewmodel/fragments/auth/factories/SignedInUserProfileViewModelFactory.kt
+++ app/src/main/java/inc/ahmedmourad/sherlock/viewmodel/fragments/auth/factories/SignedInUserProfileViewModelFactory.kt
@@ -2,14 +2,14 @@ package inc.ahmedmourad.sherlock.viewmodel.fragments.auth.factories
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
-import inc.ahmedmourad.sherlock.domain.interactors.auth.FindSignedInUserInteractor
+import inc.ahmedmourad.sherlock.domain.interactors.auth.ObserveSignedInUserInteractor
 import inc.ahmedmourad.sherlock.viewmodel.fragments.auth.SignedInUserProfileViewModel
 
 internal class SignedInUserProfileViewModelFactory(
-        private val findSignedInUserInteractor: FindSignedInUserInteractor
+        private val observeSignedInUserInteractor: ObserveSignedInUserInteractor
 ) : ViewModelProvider.NewInstanceFactory() {
     @Suppress("UNCHECKED_CAST")
     override fun <T : ViewModel> create(modelClass: Class<T>) = SignedInUserProfileViewModel(
-            findSignedInUserInteractor
+            observeSignedInUserInteractor
     ) as T
 }
