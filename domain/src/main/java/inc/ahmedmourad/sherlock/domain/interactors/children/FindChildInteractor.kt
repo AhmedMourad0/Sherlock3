2b7f232e3272c8b1599f91b50f312fb249e9f6b1
Ahmed Mourad <dev.ahmedmourad73744@gmail.com>
2020-04-08 22:21:10 +0200

    Changed parameter from SimpleRetrievedChild to ChildId

--- domain/src/main/java/inc/ahmedmourad/sherlock/domain/interactors/children/FindChildInteractor.kt
+++ domain/src/main/java/inc/ahmedmourad/sherlock/domain/interactors/children/FindChildInteractor.kt
@@ -5,17 +5,17 @@ import arrow.core.Tuple2
 import dagger.Lazy
 import inc.ahmedmourad.sherlock.domain.data.ChildrenRepository
 import inc.ahmedmourad.sherlock.domain.model.children.RetrievedChild
-import inc.ahmedmourad.sherlock.domain.model.children.SimpleRetrievedChild
 import inc.ahmedmourad.sherlock.domain.model.children.submodel.Weight
+import inc.ahmedmourad.sherlock.domain.model.ids.ChildId
 import io.reactivex.Flowable
 
 typealias FindChildInteractor =
-        (@JvmSuppressWildcards SimpleRetrievedChild) ->
+        (@JvmSuppressWildcards ChildId) ->
         @JvmSuppressWildcards Flowable<Either<Throwable, Tuple2<RetrievedChild, Weight?>?>>
 
 internal fun findChild(
         childrenRepository: Lazy<ChildrenRepository>,
-        child: SimpleRetrievedChild
+        childId: ChildId
 ): Flowable<Either<Throwable, Tuple2<RetrievedChild, Weight?>?>> {
-    return childrenRepository.get().find(child)
+    return childrenRepository.get().find(childId)
 }
