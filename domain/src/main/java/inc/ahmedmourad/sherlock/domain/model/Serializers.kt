d54043b8a041fd39e2635375dc9b1c55dd87cb8d
Ahmed Mourad <dev.ahmedmourad73744@gmail.com>
2020-04-10 02:27:39 +0200

    Updated EitherSerializer with a more robust implementation

--- domain/src/main/java/inc/ahmedmourad/sherlock/domain/model/Serializers.kt
+++ domain/src/main/java/inc/ahmedmourad/sherlock/domain/model/Serializers.kt
@@ -8,31 +8,53 @@ import kotlinx.serialization.*
 
 @Serializer(forClass = Either::class)
 class EitherSerializer<A, B>(
-        private val leftSerializer: KSerializer<A>,
-        private val rightSerializer: KSerializer<B>
-) : KSerializer<Either<A, B>> {
+        private val leftSerializer: KSerializer<A?>,
+        private val rightSerializer: KSerializer<B?>
+) : KSerializer<Either<A?, B?>> {
 
-    override val descriptor: SerialDescriptor =
-            PrimitiveDescriptor("EitherSerializer", PrimitiveKind.STRING)
-
-    override fun serialize(encoder: Encoder, value: Either<A, B>) {
-        when (value) {
-            is Either.Left -> {
-                encoder.encodeBoolean(false)
-                leftSerializer.serialize(encoder, value.a)
-            }
-            is Either.Right -> {
-                encoder.encodeBoolean(true)
-                rightSerializer.serialize(encoder, value.b)
-            }
-        }.exhaust()
+    @OptIn(ImplicitReflectionSerializer::class)
+    override val descriptor: SerialDescriptor = SerialDescriptor("arrow.core.Either") {
+        element<Boolean>("isRight")
+        element("a", leftSerializer.descriptor)
+        element("b", rightSerializer.descriptor)
     }
 
-    override fun deserialize(decoder: Decoder): Either<A, B> {
-        return if (decoder.decodeBoolean()) {
-            rightSerializer.deserialize(decoder).right()
+    override fun serialize(encoder: Encoder, value: Either<A?, B?>) {
+        val compositeOutput = encoder.beginStructure(descriptor)
+        compositeOutput.encodeBooleanElement(descriptor, 0, value.isRight())
+        when (value) {
+            is Either.Left -> {
+                compositeOutput.encodeNullableSerializableElement(descriptor, 1, leftSerializer, value.a)
+                compositeOutput.encodeNullableSerializableElement(descriptor, 2, rightSerializer, null)
+            }
+            is Either.Right -> {
+                compositeOutput.encodeNullableSerializableElement(descriptor, 1, leftSerializer, null)
+                compositeOutput.encodeNullableSerializableElement(descriptor, 2, rightSerializer, value.b)
+            }
+        }.exhaust()
+        compositeOutput.endStructure(descriptor)
+    }
+
+    override fun deserialize(decoder: Decoder): Either<A?, B?> {
+        val dec: CompositeDecoder = decoder.beginStructure(descriptor)
+        var isRight: Boolean? = null
+        var a: A? = null
+        var b: B? = null
+        loop@ while (true) {
+            when (val i = dec.decodeElementIndex(descriptor)) {
+                CompositeDecoder.READ_DONE -> break@loop
+                0 -> isRight = dec.decodeBooleanElement(descriptor, i)
+                1 -> a = dec.decodeNullableSerializableElement(descriptor, i, leftSerializer)
+                2 -> b = dec.decodeNullableSerializableElement(descriptor, i, rightSerializer)
+                else -> throw SerializationException("Unknown index: $i")
+            }
+        }
+        dec.endStructure(descriptor)
+        isRight ?: throw SerializationException("Cannot find field: isRight")
+        return if (isRight) {
+            b.right()
         } else {
-            leftSerializer.deserialize(decoder).left()
+            a.left()
         }
     }
 }
