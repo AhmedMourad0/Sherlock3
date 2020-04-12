fa3ea4456832be7b12419ac0b1766e5ac02047af
Ahmed Mourad <dev.ahmedmourad73744@gmail.com>
2020-04-12 05:29:16 +0200

    Removed defaultArgs from parameters

--- app/src/main/java/inc/ahmedmourad/sherlock/viewmodel/factory/SimpleViewModelFactoryFactory.kt
+++ app/src/main/java/inc/ahmedmourad/sherlock/viewmodel/factory/SimpleViewModelFactoryFactory.kt
@@ -1,9 +1,7 @@
 package inc.ahmedmourad.sherlock.viewmodel.factory
 
-import android.os.Bundle
 import androidx.lifecycle.AbstractSavedStateViewModelFactory
 import androidx.savedstate.SavedStateRegistryOwner
 
 internal typealias SimpleViewModelFactoryFactory =
-        (@JvmSuppressWildcards SavedStateRegistryOwner, @JvmSuppressWildcards Bundle?) ->
-        @JvmSuppressWildcards AbstractSavedStateViewModelFactory
+        (@JvmSuppressWildcards SavedStateRegistryOwner) -> @JvmSuppressWildcards AbstractSavedStateViewModelFactory
