d7d37ca9b9390d43f72c59d38474f141e973e3e5
Ahmed Mourad <dev.ahmedmourad73744@gmail.com>
2020-04-12 05:29:57 +0200

    Made PicturePath Parcelable

--- app/src/main/java/inc/ahmedmourad/sherlock/utils/pickers/images/ImagePicker.kt
+++ app/src/main/java/inc/ahmedmourad/sherlock/utils/pickers/images/ImagePicker.kt
@@ -2,6 +2,8 @@ package inc.ahmedmourad.sherlock.utils.pickers.images
 
 import android.app.Activity
 import android.content.Intent
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
 
 internal typealias OnSelect = (ImagePicker.PicturePath) -> Unit
 internal typealias OnError = (Throwable) -> Unit
@@ -12,5 +14,6 @@ internal interface ImagePicker {
 
     fun handleActivityResult(requestCode: Int, data: Intent, onSelect: OnSelect)
 
-    data class PicturePath(val value: String)
+    @Parcelize
+    data class PicturePath(val value: String) : Parcelable
 }
