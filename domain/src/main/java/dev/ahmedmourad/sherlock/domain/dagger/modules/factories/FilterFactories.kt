051b289655e931c71ea56b3ed7ff016884201926
Ahmed Mourad <dev.ahmedmourad73744@gmail.com>
2020-04-15 09:55:48 +0200

    Converted all factories from top-level functions to function classes to be able to use construction injections

--- domain/src/main/java/dev/ahmedmourad/sherlock/domain/dagger/modules/factories/FilterFactories.kt
+++ domain/src/main/java/dev/ahmedmourad/sherlock/domain/dagger/modules/factories/FilterFactories.kt
@@ -1,6 +1,8 @@
 package dev.ahmedmourad.sherlock.domain.dagger.modules.factories
 
 import dagger.Lazy
+import dagger.Reusable
+import dev.ahmedmourad.sherlock.domain.dagger.modules.qualifiers.InternalApi
 import dev.ahmedmourad.sherlock.domain.filter.Filter
 import dev.ahmedmourad.sherlock.domain.filter.ResultsFilter
 import dev.ahmedmourad.sherlock.domain.filter.criteria.Criteria
@@ -8,23 +10,28 @@ import dev.ahmedmourad.sherlock.domain.filter.criteria.LooseCriteria
 import dev.ahmedmourad.sherlock.domain.model.children.ChildQuery
 import dev.ahmedmourad.sherlock.domain.model.children.RetrievedChild
 import dev.ahmedmourad.sherlock.domain.platform.LocationManager
+import javax.inject.Inject
 
 typealias ChildrenFilterFactory =
         (@JvmSuppressWildcards ChildQuery) -> @JvmSuppressWildcards Filter<RetrievedChild>
 
-internal fun childrenFilterFactory(
-        criteriaFactory: ChildrenCriteriaFactory,
-        rules: ChildQuery
-): Filter<RetrievedChild> {
-    return ResultsFilter(criteriaFactory(rules))
+@Reusable
+internal class ChildrenFilterFactoryImpl @Inject constructor(
+        @InternalApi private val criteriaFactory: ChildrenCriteriaFactory
+) : ChildrenFilterFactory {
+    override fun invoke(query: ChildQuery): Filter<RetrievedChild> {
+        return ResultsFilter(criteriaFactory(query))
+    }
 }
 
 typealias ChildrenCriteriaFactory =
         (@JvmSuppressWildcards ChildQuery) -> @JvmSuppressWildcards Criteria<RetrievedChild>
 
-internal fun childrenLooseCriteriaFactory(
-        locationManager: Lazy<LocationManager>,
-        rules: ChildQuery
-): Criteria<RetrievedChild> {
-    return LooseCriteria(rules, locationManager)
+@Reusable
+internal class ChildrenCriteriaFactoryImpl @Inject constructor(
+        private val locationManager: Lazy<LocationManager>
+) : ChildrenCriteriaFactory {
+    override fun invoke(query: ChildQuery): Criteria<RetrievedChild> {
+        return LooseCriteria(query, locationManager)
+    }
 }
