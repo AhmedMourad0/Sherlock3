45dd4427b72f1373be95987819874b2af73f353f
Ahmed Mourad <dev.ahmedmourad73744@gmail.com>
2020-04-15 10:00:24 +0200

    Replaced old @Provides fragmented modules with the new feature-specific @Binds ones

--- domain/src/main/java/dev/ahmedmourad/sherlock/domain/dagger/DomainModule.kt
+++ domain/src/main/java/dev/ahmedmourad/sherlock/domain/dagger/DomainModule.kt
@@ -1,31 +1,13 @@
 package dev.ahmedmourad.sherlock.domain.dagger
 
 import dagger.Module
-import dev.ahmedmourad.sherlock.domain.dagger.modules.*
+import dev.ahmedmourad.sherlock.domain.dagger.modules.AuthModule
+import dev.ahmedmourad.sherlock.domain.dagger.modules.ChildrenModule
+import dev.ahmedmourad.sherlock.domain.dagger.modules.CommonModule
 
 @Module(includes = [
-    FilterModule::class,
-    BusModule::class,
-    AddChildInteractorModule::class,
-    FindChildrenInteractorModule::class,
-    FindChildInteractorModule::class,
-    FindLastSearchResultsInteractorModule::class,
-    ObserveInternetConnectivityInteractorModule::class,
-    CheckInternetConnectivityInteractorModule::class,
-    ObserveChildPublishingStateInteractorModule::class,
-    CheckChildPublishingStateInteractorModule::class,
-    NotifyChildPublishingStateChangeInteractorModule::class,
-    NotifyChildFindingStateChangeInteractorModule::class,
-    NotifyChildrenFindingStateChangeInteractorModule::class,
-    ObserveUserAuthStateInteractorModule::class,
-    ObserveSignedInUserInteractorModule::class,
-    SendPasswordResetEmailInteractorModule::class,
-    SignInInteractorModule::class,
-    SignInWithFacebookInteractorModule::class,
-    SignInWithGoogleInteractorModule::class,
-    SignInWithTwitterInteractorModule::class,
-    SignOutInteractorModule::class,
-    SignUpInteractorModule::class,
-    CompleteSignUpInteractorModule::class
+    ChildrenModule::class,
+    AuthModule::class,
+    CommonModule::class
 ])
 object DomainModule
