a6d6424e892b151be635ca69d921cb12e4c6fb92
Ahmed Mourad <dev.ahmedmourad73744@gmail.com>
2020-04-15 07:58:16 +0200

    Renamed RxBus to BusImpl and implemented constructor injection

--- domain/src/main/java/dev/ahmedmourad/sherlock/domain/bus/BusImpl.kt
+++ domain/src/main/java/dev/ahmedmourad/sherlock/domain/bus/BusImpl.kt
@@ -4,8 +4,11 @@ import com.jakewharton.rxrelay2.BehaviorRelay
 import com.jakewharton.rxrelay2.Relay
 import dev.ahmedmourad.sherlock.domain.constants.BackgroundState
 import dev.ahmedmourad.sherlock.domain.constants.PublishingState
+import javax.inject.Inject
+import javax.inject.Singleton
 
-internal class RxBus : Bus {
+@Singleton
+internal class BusImpl @Inject constructor() : Bus {
     override val childPublishingState: Relay<PublishingState> = BehaviorRelay.create()
     override val childFindingState: Relay<BackgroundState> = BehaviorRelay.create()
     override val childrenFindingState: Relay<BackgroundState> = BehaviorRelay.create()
