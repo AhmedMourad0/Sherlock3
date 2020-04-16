b70b5fa8707e624fe4697836dd946cfa31fdf8c6
Ahmed Mourad <dev.ahmedmourad73744@gmail.com>
2020-04-16 16:24:05 +0200

    Converted AuthModule from a singleton object to an interface

--- auth/src/main/java/dev/ahmedmourad/sherlock/auth/dagger/AuthModule.kt
+++ auth/src/main/java/dev/ahmedmourad/sherlock/auth/dagger/AuthModule.kt
@@ -1,7 +1,6 @@
 package dev.ahmedmourad.sherlock.auth.dagger
 
 import dagger.Module
-import dev.ahmedmourad.sherlock.auth.dagger.modules.AuthManagerModule
 
 @Module(includes = [AuthManagerModule::class])
-object AuthModule
+interface AuthModule
