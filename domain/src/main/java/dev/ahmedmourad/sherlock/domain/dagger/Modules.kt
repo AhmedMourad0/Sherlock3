eb000192476fc0f458ddf5e6a6bc31e117adb174
Ahmed Mourad <dev.ahmedmourad73744@gmail.com>
2020-04-24 17:14:33 +0200

    Combined AuthBindingsModule.kt, ChildrenBindingsModule.kt and CommonBindingsModule into Modules.kt

---
+++ domain/src/main/java/dev/ahmedmourad/sherlock/domain/dagger/Modules.kt
@@ -0,0 +1,133 @@
+package dev.ahmedmourad.sherlock.domain.dagger
+
+import dagger.Binds
+import dagger.Module
+import dev.ahmedmourad.sherlock.domain.bus.Bus
+import dev.ahmedmourad.sherlock.domain.bus.BusImpl
+import dev.ahmedmourad.sherlock.domain.interactors.auth.*
+import dev.ahmedmourad.sherlock.domain.interactors.children.*
+import dev.ahmedmourad.sherlock.domain.interactors.common.*
+
+@Module
+internal interface AuthBindingsModule {
+
+    @Binds
+    fun bindObserveUserAuthStateInteractor(
+            interactor: ObserveUserAuthStateInteractorImpl
+    ): ObserveUserAuthStateInteractor
+
+    @Binds
+    fun bindObserveSignedInUserInteractor(
+            interactor: ObserveSignedInUserInteractorImpl
+    ): ObserveSignedInUserInteractor
+
+    @Binds
+    fun bindSendPasswordResetEmailInteractor(
+            interactor: SendPasswordResetEmailInteractorImpl
+    ): SendPasswordResetEmailInteractor
+
+    @Binds
+    fun bindSignInInteractor(
+            interactor: SignInInteractorImpl
+    ): SignInInteractor
+
+    @Binds
+    fun bindSignInWithFacebookInteractor(
+            interactor: SignInWithFacebookInteractorImpl
+    ): SignInWithFacebookInteractor
+
+    @Binds
+    fun bindSignInWithGoogleInteractor(
+            interactor: SignInWithGoogleInteractorImpl
+    ): SignInWithGoogleInteractor
+
+    @Binds
+    fun bindSignInWithTwitterInteractor(
+            interactor: SignInWithTwitterInteractorImpl
+    ): SignInWithTwitterInteractor
+
+    @Binds
+    fun bindSignOutInteractor(
+            interactor: SignOutInteractorImpl
+    ): SignOutInteractor
+
+    @Binds
+    fun bindSignUpInteractor(
+            interactor: SignUpInteractorImpl
+    ): SignUpInteractor
+
+    @Binds
+    fun bindCompleteSignUpInteractor(
+            interactor: CompleteSignUpInteractorImpl
+    ): CompleteSignUpInteractor
+}
+
+@Module
+internal interface ChildrenBindingsModule {
+
+    @Binds
+    fun bindAddChildInteractor(
+            interactor: AddChildInteractorImpl
+    ): AddChildInteractor
+
+    @Binds
+    fun bindFindChildrenInteractor(
+            interactor: FindChildrenInteractorImpl
+    ): FindChildrenInteractor
+
+    @Binds
+    fun bindFindChildInteractor(
+            interactor: FindChildInteractorImpl
+    ): FindChildInteractor
+
+    @Binds
+    fun bindFindLastSearchResultsInteractor(
+            interactor: FindLastSearchResultsInteractorImpl
+    ): FindLastSearchResultsInteractor
+
+    @Binds
+    fun bindObserveChildPublishingStateInteractor(
+            interactor: ObserveChildPublishingStateInteractorImpl
+    ): ObserveChildPublishingStateInteractor
+
+    @Binds
+    fun bindNotifyChildPublishingStateChangeInteractor(
+            interactor: NotifyChildPublishingStateChangeInteractorImpl
+    ): NotifyChildPublishingStateChangeInteractor
+
+    @Binds
+    fun bindNotifyChildFindingStateChangeInteractor(
+            interactor: NotifyChildFindingStateChangeInteractorImpl
+    ): NotifyChildFindingStateChangeInteractor
+
+    @Binds
+    fun bindNotifyChildrenFindingStateChangeInteractor(
+            interactor: NotifyChildrenFindingStateChangeInteractorImpl
+    ): NotifyChildrenFindingStateChangeInteractor
+
+    @Binds
+    @InternalApi
+    fun bindChildrenCriteriaFactory(
+            factory: ChildrenCriteriaFactoryImpl
+    ): ChildrenCriteriaFactory
+
+    @Binds
+    fun bindChildrenFilterFactory(
+            factory: ChildrenFilterFactoryImpl
+    ): ChildrenFilterFactory
+}
+
+@Module
+internal interface CommonBindingsModule {
+
+    @Binds
+    fun bindObserveInternetConnectivityInteractor(
+            interactor: ObserveInternetConnectivityInteractorImpl
+    ): ObserveInternetConnectivityInteractor
+
+    @Binds
+    @InternalApi
+    fun bindBus(
+            bus: BusImpl
+    ): Bus
+}
