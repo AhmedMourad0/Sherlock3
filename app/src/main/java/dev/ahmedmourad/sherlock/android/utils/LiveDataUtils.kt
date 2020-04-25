95979f0736b3ea020df64e841f3860f6971d57d5
Ahmed Mourad <dev.ahmedmourad73744@gmail.com>
2020-04-25 02:13:54 +0200

    Added observe as an extension function on LifecycleOwner

--- app/src/main/java/dev/ahmedmourad/sherlock/android/utils/LiveDataUtils.kt
+++ app/src/main/java/dev/ahmedmourad/sherlock/android/utils/LiveDataUtils.kt
@@ -1,7 +1,13 @@
 package dev.ahmedmourad.sherlock.android.utils
 
+import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.LiveDataReactiveStreams
+import androidx.lifecycle.observe
 import org.reactivestreams.Publisher
 
 fun <T> Publisher<T>.toLiveData(): LiveData<T> = LiveDataReactiveStreams.fromPublisher(this)
+
+inline fun <T> LifecycleOwner.observe(liveData: LiveData<T>, crossinline observer: (T) -> Unit) {
+    liveData.observe(this, observer)
+}
