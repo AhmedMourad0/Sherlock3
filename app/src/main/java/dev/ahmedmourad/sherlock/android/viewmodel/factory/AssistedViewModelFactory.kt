8ea8c455d876eb405a1c3fa818a526b49414f4a9
Ahmed Mourad <dev.ahmedmourad73744@gmail.com>
2020-04-26 05:30:00 +0200

    Created AssistedViewModelFactory.kt containing AssistedViewModelFactory and SimpleSavedStateViewModelFactory

---
+++ app/src/main/java/dev/ahmedmourad/sherlock/android/viewmodel/factory/AssistedViewModelFactory.kt
@@ -0,0 +1,24 @@
+package dev.ahmedmourad.sherlock.android.viewmodel.factory
+
+import android.os.Bundle
+import androidx.lifecycle.AbstractSavedStateViewModelFactory
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.savedstate.SavedStateRegistryOwner
+
+internal interface AssistedViewModelFactory<out T : ViewModel> : (SavedStateHandle) -> T
+
+internal class SimpleSavedStateViewModelFactory<out VM : ViewModel>(
+        owner: SavedStateRegistryOwner,
+        private val viewModelFactory: AssistedViewModelFactory<VM>,
+        defaultArgs: Bundle?
+) : AbstractSavedStateViewModelFactory(owner, defaultArgs) {
+    @Suppress("UNCHECKED_CAST")
+    override fun <T : ViewModel> create(
+            key: String,
+            modelClass: Class<T>,
+            handle: SavedStateHandle
+    ): T {
+        return viewModelFactory(handle) as T
+    }
+}
