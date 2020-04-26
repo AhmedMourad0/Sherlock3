86d232bb99ef89e04c78cb01964e2e5bf8a32f31
Ahmed Mourad <dev.ahmedmourad73744@gmail.com>
2020-04-26 05:26:40 +0200

    Added ViewModels bindings

--- app/src/main/java/dev/ahmedmourad/sherlock/android/di/Modules.kt
+++ app/src/main/java/dev/ahmedmourad/sherlock/android/di/Modules.kt
@@ -2,13 +2,27 @@ package dev.ahmedmourad.sherlock.android.di
 
 import dagger.Binds
 import dagger.Module
-import dev.ahmedmourad.sherlock.android.di.modules.factories.*
+import dev.ahmedmourad.sherlock.android.adapters.AppSectionsRecyclerAdapterFactory
+import dev.ahmedmourad.sherlock.android.adapters.AppSectionsRecyclerAdapterFactoryImpl
+import dev.ahmedmourad.sherlock.android.adapters.ChildrenRecyclerAdapterFactory
+import dev.ahmedmourad.sherlock.android.adapters.ChildrenRecyclerAdapterFactoryImpl
+import dev.ahmedmourad.sherlock.android.services.SherlockServiceIntentFactory
+import dev.ahmedmourad.sherlock.android.services.SherlockServiceIntentFactoryImpl
 import dev.ahmedmourad.sherlock.android.utils.formatter.TextFormatter
 import dev.ahmedmourad.sherlock.android.utils.formatter.TextFormatterImpl
 import dev.ahmedmourad.sherlock.android.utils.pickers.images.EsafirmImagePicker
 import dev.ahmedmourad.sherlock.android.utils.pickers.images.ImagePicker
 import dev.ahmedmourad.sherlock.android.utils.pickers.places.GooglePlacePicker
 import dev.ahmedmourad.sherlock.android.utils.pickers.places.PlacePicker
+import dev.ahmedmourad.sherlock.android.viewmodel.activity.MainActivityViewModel
+import dev.ahmedmourad.sherlock.android.viewmodel.factory.AssistedViewModelFactory
+import dev.ahmedmourad.sherlock.android.viewmodel.fragments.auth.*
+import dev.ahmedmourad.sherlock.android.viewmodel.fragments.children.*
+import dev.ahmedmourad.sherlock.android.viewmodel.shared.GlobalViewModel
+import dev.ahmedmourad.sherlock.android.widget.adapter.ChildrenRemoteViewsFactoryFactory
+import dev.ahmedmourad.sherlock.android.widget.adapter.ChildrenRemoteViewsFactoryFactoryImpl
+import dev.ahmedmourad.sherlock.android.widget.adapter.ChildrenRemoteViewsServiceIntentFactory
+import dev.ahmedmourad.sherlock.android.widget.adapter.ChildrenRemoteViewsServiceIntentFactoryImpl
 
 @Module
 internal interface AppBindingsModule {
@@ -19,10 +33,9 @@ internal interface AppBindingsModule {
     ): ChildrenRecyclerAdapterFactory
 
     @Binds
-    fun bindChildrenRecyclerAdapterFactory(
+    fun bindAppSectionsRecyclerAdapterFactory(
             factory: AppSectionsRecyclerAdapterFactoryImpl
     ): AppSectionsRecyclerAdapterFactory
-
 
     @Binds
     fun bindSherlockServiceIntentFactory(
@@ -53,4 +66,64 @@ internal interface AppBindingsModule {
     fun bindChildrenRemoteViewsFactoryFactory(
             factory: ChildrenRemoteViewsFactoryFactoryImpl
     ): ChildrenRemoteViewsFactoryFactory
+
+    @Binds
+    fun bindGlobalViewModel(
+            factory: GlobalViewModel.Factory
+    ): AssistedViewModelFactory<GlobalViewModel>
+
+    @Binds
+    fun bindMainActivityViewModel(
+            factory: MainActivityViewModel.Factory
+    ): AssistedViewModelFactory<MainActivityViewModel>
+
+    @Binds
+    fun bindAddChildViewModel(
+            factory: AddChildViewModel.Factory
+    ): AssistedViewModelFactory<AddChildViewModel>
+
+    @Binds
+    fun bindHomeViewModel(
+            factory: HomeViewModel.Factory
+    ): AssistedViewModelFactory<HomeViewModel>
+
+    @Binds
+    fun bindFindChildrenViewModel(
+            factory: FindChildrenViewModel.Factory
+    ): AssistedViewModelFactory<FindChildrenViewModel>
+
+    @Binds
+    fun bindResetPasswordViewModel(
+            factory: ResetPasswordViewModel.Factory
+    ): AssistedViewModelFactory<ResetPasswordViewModel>
+
+    @Binds
+    fun bindSignedInUserProfileViewModel(
+            factory: SignedInUserProfileViewModel.Factory
+    ): AssistedViewModelFactory<SignedInUserProfileViewModel>
+
+    @Binds
+    fun bindSignInViewModel(
+            factory: SignInViewModel.Factory
+    ): AssistedViewModelFactory<SignInViewModel>
+
+    @Binds
+    fun bindSignUpViewModel(
+            factory: SignUpViewModel.Factory
+    ): AssistedViewModelFactory<SignUpViewModel>
+
+    @Binds
+    fun bindCompleteSignUpViewModel(
+            factory: CompleteSignUpViewModel.Factory
+    ): AssistedViewModelFactory<CompleteSignUpViewModel>
+
+    @Binds
+    fun bindChildrenSearchResultsViewModel(
+            factory: ChildrenSearchResultsViewModel.Factory
+    ): AssistedViewModelFactory<ChildrenSearchResultsViewModel>
+
+    @Binds
+    fun bindChildDetailsViewModel(
+            factory: ChildDetailsViewModel.Factory
+    ): AssistedViewModelFactory<ChildDetailsViewModel>
 }
