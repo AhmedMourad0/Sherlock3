f1fd9a3c601926a5c27f5a106733dd6bff826c7e
Ahmed Mourad <dev.ahmedmourad73744@gmail.com>
2020-04-26 05:29:12 +0200

    Deleted SimpleViewModelFactoryFactory.kt

--- app/src/main/java/dev/ahmedmourad/sherlock/android/viewmodel/factory/SimpleViewModelFactoryFactory.kt
+++
@@ -1,7 +0,0 @@
-package dev.ahmedmourad.sherlock.android.viewmodel.factory
-
-import androidx.lifecycle.AbstractSavedStateViewModelFactory
-import androidx.savedstate.SavedStateRegistryOwner
-
-internal typealias SimpleViewModelFactoryFactory =
-        (@JvmSuppressWildcards SavedStateRegistryOwner) -> @JvmSuppressWildcards AbstractSavedStateViewModelFactory
