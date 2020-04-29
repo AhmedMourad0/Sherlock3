a3c3986a708803af46ca656defbecea98fced091
Ahmed Mourad <dev.ahmedmourad73744@gmail.com>
2020-04-29 22:20:03 +0200

    Cleared meaningless comments

--- app/src/main/java/dev/ahmedmourad/sherlock/android/application/SherlockApplication.kt
+++ app/src/main/java/dev/ahmedmourad/sherlock/android/application/SherlockApplication.kt
@@ -15,31 +15,3 @@ internal class SherlockApplication : MultiDexApplication(), DaggerComponentProvi
         Timber.plant(LogcatTree("Sherlock"))
     }
 }
-
-/*
-*
-* Work on the ui (the 3 retry options too, the (AddChildController, DisplayChildController, SearchResultsController) progress, no items in lists, place picker, maybe separate it into separate object as well), coordinator layout for the connection indicator
-*
-* Use Timber to its full potential (Crashlytics)
-*
-* Dagger encapsulation with qualifiers
-*
-* * Package per feature
-*
-* replace exceptions with ADTs in interactors
-*
-* replace RxJava and Coroutines Flow with Monads and Functors for more abstraction
-*
-* The Dagger refactoring stage (Qualifier, Binding, SingleComponent, providers, Kotlin initiative)
-*
-* testing
-*
-*  / ** Done ** /
-*
-* ConstraintLayout
-*
-* Kotlin Flows and Coroutines
-*
-* Kotlin Multiplatform
-*
-* */
