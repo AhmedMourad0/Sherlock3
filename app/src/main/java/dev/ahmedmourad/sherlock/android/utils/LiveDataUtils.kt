f8894ad3ebdb49df458a7658997fbdf9374550e8
Ahmed Mourad <dev.ahmedmourad73744@gmail.com>
2020-04-29 14:30:11 +0200

    Added observeAll to enable attaching the same observer to multiple viewmodels

--- app/src/main/java/dev/ahmedmourad/sherlock/android/utils/LiveDataUtils.kt
+++ app/src/main/java/dev/ahmedmourad/sherlock/android/utils/LiveDataUtils.kt
@@ -1,10 +1,7 @@
 package dev.ahmedmourad.sherlock.android.utils
 
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.LifecycleOwner
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.LiveDataReactiveStreams
-import androidx.lifecycle.observe
+import androidx.lifecycle.*
 import org.reactivestreams.Publisher
 
 fun <T> Publisher<T>.toLiveData(): LiveData<T> = LiveDataReactiveStreams.fromPublisher(this)
@@ -13,7 +10,18 @@ inline fun <T> LifecycleOwner.observe(liveData: LiveData<T>, crossinline observe
     liveData.observe(this, observer)
 }
 
+inline fun <T> LifecycleOwner.observeAll(vararg liveData: LiveData<out T>, crossinline observer: (T) -> Unit) {
+    val mediatorLiveData = MediatorLiveData<T>()
+    liveData.forEach { mediatorLiveData.addSource(it, mediatorLiveData::setValue) }
+    mediatorLiveData.observe(this, observer)
+}
+
 //This prevents us from accidentally calling observe with the fragment itself as the lifecycle owner
 inline fun <T> Fragment.observe(liveData: LiveData<T>, crossinline observer: (T) -> Unit) {
-    viewLifecycleOwner.observe(liveData, observer)
+    viewLifecycleOwner.observe(liveData, observer = observer)
+}
+
+//This prevents us from accidentally calling observe with the fragment itself as the lifecycle owner
+inline fun <T> Fragment.observeAll(vararg liveData: LiveData<out T>, crossinline observer: (T) -> Unit) {
+    viewLifecycleOwner.observeAll(*liveData, observer = observer)
 }
