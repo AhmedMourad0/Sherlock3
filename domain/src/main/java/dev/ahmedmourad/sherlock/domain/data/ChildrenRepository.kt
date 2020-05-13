42102815c1a694f4ec6c9cf9cf857dac28d2dc8c
Ahmed Mourad <dev.ahmedmourad73744@gmail.com>
2020-05-14 00:21:50 +0200

    Replaced all exceptions with ADTs

--- domain/src/main/java/dev/ahmedmourad/sherlock/domain/data/ChildrenRepository.kt
+++ domain/src/main/java/dev/ahmedmourad/sherlock/domain/data/ChildrenRepository.kt
@@ -14,22 +14,54 @@ import io.reactivex.Single
 
 interface ChildrenRepository {
 
-    fun publish(child: PublishedChild): Single<Either<Throwable, RetrievedChild>>
+    fun publish(child: PublishedChild): Single<Either<PublishException, RetrievedChild>>
 
     fun find(
             childId: ChildId
-    ): Flowable<Either<Throwable, Tuple2<RetrievedChild, Weight?>?>>
+    ): Flowable<Either<FindException, Tuple2<RetrievedChild, Weight?>?>>
 
     fun findAll(
             query: ChildQuery,
             filter: Filter<RetrievedChild>
-    ): Flowable<Either<Throwable, Map<SimpleRetrievedChild, Weight>>>
+    ): Flowable<Either<FindAllException, Map<SimpleRetrievedChild, Weight>>>
 
-    fun findLastSearchResults(): Flowable<Either<Throwable, Map<SimpleRetrievedChild, Weight>>>
+    fun findLastSearchResults(): Flowable<Either<FindLastSearchResultsException, Map<SimpleRetrievedChild, Weight>>>
 
     fun test(): Tester
 
     interface Tester {
-        fun clear(): Single<Either<Throwable, Unit>>
+
+        fun clear(): Single<Either<ClearException, Unit>>
+
+        sealed class ClearException {
+            object NoInternetConnectionException : ClearException()
+            object NoSignedInUserException : ClearException()
+            data class UnknownException(val origin: Throwable) : ClearException()
+        }
+    }
+
+    sealed class PublishException {
+        object NoInternetConnectionException : PublishException()
+        object NoSignedInUserException : PublishException()
+        data class InternalException(val origin: Throwable) : PublishException()
+        data class UnknownException(val origin: Throwable) : PublishException()
+    }
+
+    sealed class FindException {
+        object NoInternetConnectionException : FindException()
+        object NoSignedInUserException : FindException()
+        data class InternalException(val origin: Throwable) : FindException()
+        data class UnknownException(val origin: Throwable) : FindException()
+    }
+
+    sealed class FindAllException {
+        object NoInternetConnectionException : FindAllException()
+        object NoSignedInUserException : FindAllException()
+        data class UnknownException(val origin: Throwable) : FindAllException()
+    }
+
+    sealed class FindLastSearchResultsException {
+        data class InternalException(val origin: Throwable) : FindLastSearchResultsException()
+        data class UnknownException(val origin: Throwable) : FindLastSearchResultsException()
     }
 }
