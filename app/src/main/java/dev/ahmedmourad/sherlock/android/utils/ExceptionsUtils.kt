e33126a90edd94a8ffe37c2b218393e1ad6fd326
Ahmed Mourad <dev.ahmedmourad73744@gmail.com>
2020-05-16 10:11:02 +0200

    Function `somethingWentWrong` now accepts null as an argument

--- app/src/main/java/dev/ahmedmourad/sherlock/android/utils/ExceptionsUtils.kt
+++ app/src/main/java/dev/ahmedmourad/sherlock/android/utils/ExceptionsUtils.kt
@@ -3,8 +3,8 @@ package dev.ahmedmourad.sherlock.android.utils
 import dev.ahmedmourad.sherlock.android.R
 import splitties.init.appCtx
 
-internal fun somethingWentWrong(e: Throwable): String {
-    return e.message?.let { msg ->
+internal fun somethingWentWrong(e: Throwable? = null): String {
+    return e?.message?.let { msg ->
         appCtx.getString(R.string.something_went_wrong_with_message, msg)
     } ?: appCtx.getString(R.string.something_went_wrong)
 }
