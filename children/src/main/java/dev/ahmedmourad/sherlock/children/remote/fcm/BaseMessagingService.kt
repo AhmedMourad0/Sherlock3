62e8b900c21885ede72dc4de9ed69d373d7c2842
Ahmed Mourad <dev.ahmedmourad73744@gmail.com>
2020-09-04 14:19:16 +0200

    Created BaseMessagingService.kt

---
+++ children/src/main/java/dev/ahmedmourad/sherlock/children/remote/fcm/BaseMessagingService.kt
@@ -0,0 +1,25 @@
+package dev.ahmedmourad.sherlock.children.remote.fcm
+
+import android.annotation.SuppressLint
+import arrow.core.orNull
+import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
+import dev.ahmedmourad.sherlock.domain.model.children.submodel.Weight
+import dev.ahmedmourad.sherlock.domain.model.ids.ChildId
+
+@SuppressLint("MissingFirebaseInstanceTokenRefresh")
+abstract class BaseMessagingService : FirebaseMessagingService() {
+
+    override fun onMessageReceived(remoteMessage: RemoteMessage) {
+        if (remoteMessage.data.isNotEmpty()) {
+            val id = remoteMessage.data["id"]?.let(::ChildId) ?: return
+            val weight = remoteMessage.data["weight"]
+                    ?.toDoubleOrNull()
+                    ?.let(Weight.Companion::of)
+                    ?.orNull() ?: return
+            handleMatchingChildFound(id, weight)
+        }
+    }
+
+    abstract fun handleMatchingChildFound(id: ChildId, weight: Weight)
+}
