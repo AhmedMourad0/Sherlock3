7157f726660e98299ba936c4fa1b3d7870f04458
Ahmed Mourad <dev.ahmedmourad73744@gmail.com>
2020-09-07 09:44:35 +0200

    Added the time and hashing utility functions

--- domain/src/main/java/dev/ahmedmourad/sherlock/domain/utils/CommonUtils.kt
+++ domain/src/main/java/dev/ahmedmourad/sherlock/domain/utils/CommonUtils.kt
@@ -1,3 +1,18 @@
 package dev.ahmedmourad.sherlock.domain.utils
 
+import kotlinx.datetime.*
+import java.security.MessageDigest
+
 fun <T> T.exhaust() = this
+
+fun localDateTime(millis: Long): LocalDateTime {
+    return Instant.fromEpochMilliseconds(millis).toLocalDateTime(TimeZone.currentSystemDefault())
+}
+
+fun LocalDateTime.toMillis(): Long {
+    return this.toInstant(TimeZone.currentSystemDefault()).toEpochMilliseconds()
+}
+
+fun hash(str: String): ByteArray {
+    return MessageDigest.getInstance("MD5").digest(str.toByteArray())
+}
