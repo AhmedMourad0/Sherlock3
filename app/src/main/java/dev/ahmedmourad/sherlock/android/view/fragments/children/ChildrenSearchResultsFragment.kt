d1c86d7534b16b52413d7df75d91598a2cb10862
Ahmed Mourad <dev.ahmedmourad73744@gmail.com>
2020-09-07 09:57:22 +0200

    Fixed the responsibilities of both ChildrenSearchResultsFragment and ChildrenSearchResultsViewModel

--- app/src/main/java/dev/ahmedmourad/sherlock/android/view/fragments/children/ChildrenSearchResultsFragment.kt
+++ app/src/main/java/dev/ahmedmourad/sherlock/android/view/fragments/children/ChildrenSearchResultsFragment.kt
@@ -5,11 +5,11 @@ import android.view.View
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import dagger.Lazy
 import dev.ahmedmourad.bundlizer.bundle
 import dev.ahmedmourad.bundlizer.unbundle
 import dev.ahmedmourad.sherlock.android.R
@@ -17,31 +17,20 @@ import dev.ahmedmourad.sherlock.android.adapters.ChildrenRecyclerAdapterFactory
 import dev.ahmedmourad.sherlock.android.adapters.DynamicRecyclerAdapter
 import dev.ahmedmourad.sherlock.android.databinding.FragmentChildrenSearchResultsBinding
 import dev.ahmedmourad.sherlock.android.di.injector
-import dev.ahmedmourad.sherlock.android.formatter.TextFormatter
-import dev.ahmedmourad.sherlock.android.interpreters.interactors.localizedMessage
 import dev.ahmedmourad.sherlock.android.utils.observe
 import dev.ahmedmourad.sherlock.android.view.BackdropActivity
 import dev.ahmedmourad.sherlock.android.viewmodel.factory.AssistedViewModelFactory
 import dev.ahmedmourad.sherlock.android.viewmodel.factory.SimpleSavedStateViewModelFactory
 import dev.ahmedmourad.sherlock.android.viewmodel.fragments.children.ChildrenSearchResultsViewModel
-import dev.ahmedmourad.sherlock.domain.interactors.children.FindChildrenInteractor
-import dev.ahmedmourad.sherlock.domain.model.children.ChildQuery
+import dev.ahmedmourad.sherlock.domain.model.children.ChildrenQuery
 import dev.ahmedmourad.sherlock.domain.model.children.SimpleRetrievedChild
 import dev.ahmedmourad.sherlock.domain.model.children.submodel.Weight
-import dev.ahmedmourad.sherlock.domain.platform.DateManager
 import dev.ahmedmourad.sherlock.domain.utils.exhaust
-import timber.log.Timber
-import timber.log.error
+import splitties.init.appCtx
 import javax.inject.Inject
 import javax.inject.Provider
 
 internal class ChildrenSearchResultsFragment : Fragment(R.layout.fragment_children_search_results) {
-
-    @Inject
-    internal lateinit var dateManager: Lazy<DateManager>
-
-    @Inject
-    internal lateinit var textFormatter: Lazy<TextFormatter>
 
     @Inject
     internal lateinit var adapterFactory: ChildrenRecyclerAdapterFactory
@@ -55,7 +44,7 @@ internal class ChildrenSearchResultsFragment : Fragment(R.layout.fragment_childr
         SimpleSavedStateViewModelFactory(
                 this,
                 viewModelFactory,
-                ChildrenSearchResultsViewModel.defaultArgs(args.query.unbundle(ChildQuery.serializer()))
+                ChildrenSearchResultsViewModel.defaultArgs(args.query.unbundle(ChildrenQuery.serializer()))
         )
     }
 
@@ -74,29 +63,117 @@ internal class ChildrenSearchResultsFragment : Fragment(R.layout.fragment_childr
 
         //TODO: either give the option to update or not, or onPublish new values to the bottom
         //TODO: paginate
-        //TODO: show loading, hide when date is received
-        observe(viewModel.searchResults) { resultsEither ->
-            resultsEither.fold(ifLeft = { e ->
-                //TODO: show error image with retry option when appropriate
-                @Suppress("IMPLICIT_CAST_TO_ANY")
-                when (e) {
+        observe(viewModel.state, Observer { state ->
+            when (state) {
 
-                    FindChildrenInteractor.Exception.NoInternetConnectionException -> Unit
-
-                    FindChildrenInteractor.Exception.NoSignedInUserException -> {
-                        (requireActivity() as BackdropActivity).setInPrimaryContentMode(false)
+                is ChildrenSearchResultsViewModel.State.Data -> {
+                    adapter.update(state.items)
+                    binding?.let { b ->
+                        b.recycler.visibility = View.VISIBLE
+                        b.startInvestigation.show()
+                        b.error.root.visibility = View.GONE
+                        b.loading.root.visibility = View.GONE
                     }
+                }
 
-                    is FindChildrenInteractor.Exception.InternalException -> {
-                        Timber.error(RuntimeException(e.toString()), e::toString)
+                ChildrenSearchResultsViewModel.State.NoData -> {
+                    adapter.update(emptyMap())
+                    binding?.let { b ->
+                        b.recycler.visibility = View.GONE
+                        b.startInvestigation.hide()
+                        b.loading.root.visibility = View.GONE
+                        b.error.root.visibility = View.VISIBLE
+                        b.error.errorMessage.setText(R.string.have_no_ongoing_investigations)
+                        b.error.errorIcon.setImageResource(R.drawable.ic_detective_woman)
                     }
+                }
 
-                    is FindChildrenInteractor.Exception.UnknownException -> {
-                        Timber.error(RuntimeException(e.toString()), e::toString)
+                ChildrenSearchResultsViewModel.State.Loading -> {
+                    binding?.let { b ->
+                        b.recycler.visibility = View.GONE
+                        b.startInvestigation.hide()
+                        b.error.root.visibility = View.GONE
+                        b.loading.root.visibility = View.VISIBLE
                     }
-                }.exhaust()
-                Toast.makeText(context, e.localizedMessage(), Toast.LENGTH_LONG).show()
-            }, ifRight = adapter::update)
+                }
+
+                ChildrenSearchResultsViewModel.State.NoInternet -> {
+                    (requireActivity() as BackdropActivity).setInPrimaryContentMode(true)
+                    binding?.let { b ->
+                        b.recycler.visibility = View.GONE
+                        b.startInvestigation.hide()
+                        b.loading.root.visibility = View.GONE
+                        b.error.root.visibility = View.VISIBLE
+                        b.error.errorMessage.setText(R.string.no_internet_connection)
+                        b.error.errorIcon.setImageResource(R.drawable.ic_no_internet_colorful)
+                    }
+                }
+
+                ChildrenSearchResultsViewModel.State.NoSignedInUser -> {
+                    (requireActivity() as BackdropActivity).setInPrimaryContentMode(false)
+                    binding?.let { b ->
+                        b.recycler.visibility = View.GONE
+                        b.startInvestigation.hide()
+                        b.loading.root.visibility = View.GONE
+                        b.error.root.visibility = View.VISIBLE
+                        b.error.errorMessage.setText(R.string.sign_in_needed_to_view)
+                        b.error.errorIcon.setImageResource(R.drawable.ic_finger_print)
+                    }
+                    Toast.makeText(
+                            appCtx,
+                            R.string.authentication_needed,
+                            Toast.LENGTH_LONG
+                    ).show()
+                }
+
+                ChildrenSearchResultsViewModel.State.Error -> {
+                    binding?.let { b ->
+                        b.recycler.visibility = View.GONE
+                        b.startInvestigation.hide()
+                        b.loading.root.visibility = View.GONE
+                        b.error.root.visibility = View.VISIBLE
+                        b.error.errorMessage.setText(R.string.something_went_wrong)
+                        b.error.errorIcon.setImageResource(R.drawable.ic_research)
+                    }
+                }
+            }.exhaust()
+        })
+
+        observe(viewModel.startInvestigationSuccess, Observer {
+            binding?.startInvestigation?.hide()
+            Toast.makeText(
+                    appCtx,
+                    getString(R.string.start_investigation_success),
+                    Toast.LENGTH_LONG
+            ).show()
+            viewModel.onStartInvestigationSuccessHandled()
+        })
+
+        observe(viewModel.startInvestigationError, Observer { msg ->
+            if (msg != null) {
+                Toast.makeText(
+                        appCtx,
+                        msg,
+                        Toast.LENGTH_LONG
+                ).show()
+            } else {
+                Toast.makeText(
+                        appCtx,
+                        R.string.something_went_wrong,
+                        Toast.LENGTH_LONG
+                ).show()
+            }
+            viewModel.onStartInvestigationErrorHandled()
+            binding?.startInvestigation?.isEnabled = true
+        })
+
+        binding?.startInvestigation?.setOnClickListener {
+            viewModel.onStartInvestigation()
+            binding?.startInvestigation?.isEnabled = false
+        }
+
+        binding?.error?.root?.setOnClickListener {
+            viewModel.onRefresh()
         }
     }
 
@@ -114,6 +191,16 @@ internal class ChildrenSearchResultsFragment : Fragment(R.layout.fragment_childr
             b.recycler.layoutManager = LinearLayoutManager(context, RecyclerView.VERTICAL, false)
             b.recycler.isVerticalScrollBarEnabled = true
         }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        viewModel.onRefresh()
+    }
+
+    override fun onStop() {
+        viewModel.onInvalidateAllQueries()
+        super.onStop()
     }
 
     override fun onDestroyView() {
--- app/src/main/java/dev/ahmedmourad/sherlock/android/viewmodel/fragments/children/ChildrenSearchResultsViewModel.kt
+++ app/src/main/java/dev/ahmedmourad/sherlock/android/viewmodel/fragments/children/ChildrenSearchResultsViewModel.kt
@@ -1,51 +1,160 @@
 package dev.ahmedmourad.sherlock.android.viewmodel.fragments.children
 
 import android.os.Bundle
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.Transformations
 import androidx.lifecycle.ViewModel
 import dagger.Lazy
 import dagger.Reusable
 import dev.ahmedmourad.bundlizer.bundle
 import dev.ahmedmourad.bundlizer.unbundle
+import dev.ahmedmourad.sherlock.android.interpreters.interactors.localizedMessage
+import dev.ahmedmourad.sherlock.android.utils.somethingWentWrong
 import dev.ahmedmourad.sherlock.android.utils.toLiveData
 import dev.ahmedmourad.sherlock.android.viewmodel.factory.AssistedViewModelFactory
-import dev.ahmedmourad.sherlock.domain.filter.ChildrenFilterFactory
+import dev.ahmedmourad.sherlock.domain.interactors.children.AddInvestigationInteractor
 import dev.ahmedmourad.sherlock.domain.interactors.children.FindChildrenInteractor
-import dev.ahmedmourad.sherlock.domain.model.children.ChildQuery
+import dev.ahmedmourad.sherlock.domain.interactors.children.InvalidateAllQueriesInteractor
+import dev.ahmedmourad.sherlock.domain.model.children.ChildrenQuery
+import dev.ahmedmourad.sherlock.domain.model.children.Investigation
+import dev.ahmedmourad.sherlock.domain.model.children.SimpleRetrievedChild
+import dev.ahmedmourad.sherlock.domain.model.children.submodel.Weight
 import io.reactivex.BackpressureStrategy
+import io.reactivex.Flowable
 import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.subjects.PublishSubject
+import kotlinx.serialization.builtins.nullable
+import timber.log.Timber
+import timber.log.error
 import javax.inject.Inject
 import javax.inject.Provider
 
 internal class ChildrenSearchResultsViewModel(
-        @Suppress("UNUSED_PARAMETER") savedStateHandle: SavedStateHandle,
-        interactor: Lazy<FindChildrenInteractor>,
-        filterFactory: Lazy<ChildrenFilterFactory>
+        private val savedStateHandle: SavedStateHandle,
+        findChildrenInteractor: Lazy<FindChildrenInteractor>,
+        private val addInvestigationInteractor: Lazy<AddInvestigationInteractor>,
+        private val invalidateAllQueriesInteractor: Lazy<InvalidateAllQueriesInteractor>
 ) : ViewModel() {
 
-    private val query: ChildQuery =
-            savedStateHandle.get<Bundle>(KEY_CHILD_QUERY)!!.unbundle(ChildQuery.serializer())
+    private val compositeDisposable = CompositeDisposable()
+
+    private val query: ChildrenQuery =
+            savedStateHandle.get<Bundle>(KEY_CHILD_QUERY)!!.unbundle(ChildrenQuery.serializer())
+
+    val startInvestigationError: LiveData<String?>
+            by lazy { savedStateHandle.getLiveData<String?>(KEY_START_INVESTIGATION_ERROR, null) }
+
+    val startInvestigationSuccess: LiveData<Investigation?> by lazy {
+        Transformations.map(savedStateHandle.getLiveData<Bundle?>(KEY_START_INVESTIGATION_SUCCESS, null)) {
+            it?.unbundle(Investigation.serializer().nullable)
+        }
+    }
+
+    fun onStartInvestigationErrorHandled() {
+        savedStateHandle.set(KEY_START_INVESTIGATION_ERROR, null)
+    }
+
+    fun onStartInvestigationSuccessHandled() {
+        savedStateHandle.set(KEY_START_INVESTIGATION_SUCCESS, null)
+    }
 
     private val refreshSubject = PublishSubject.create<Unit>()
 
-    val searchResults by lazy {
-        interactor.get()
-                .invoke(query, filterFactory.get().invoke(query))
-                .retryWhen { refreshSubject.toFlowable(BackpressureStrategy.LATEST) }
+    val state by lazy {
+        Flowable.just<State>(State.Loading)
+                .concatWith(findChildrenInteractor.get()
+                        .invoke(query)
+                        .map { resultsEither ->
+                            resultsEither.fold<State>(ifLeft = { e ->
+                                when (e) {
+
+                                    FindChildrenInteractor.Exception.NoInternetConnectionException -> {
+                                        State.NoInternet
+                                    }
+
+                                    FindChildrenInteractor.Exception.NoSignedInUserException -> {
+                                        State.NoSignedInUser
+                                    }
+
+                                    is FindChildrenInteractor.Exception.InternalException -> {
+                                        Timber.error(message = e::toString)
+                                        State.Error
+                                    }
+
+                                    is FindChildrenInteractor.Exception.UnknownException -> {
+                                        Timber.error(message = e::toString)
+                                        State.Error
+                                    }
+                                }
+                            }, ifRight = { list ->
+                                if (list.isEmpty()) {
+                                    State.NoData
+                                } else {
+                                    State.Data(list)
+                                }
+                            })
+                        }
+                ).retryWhen { refreshSubject.toFlowable(BackpressureStrategy.LATEST) }
                 .observeOn(AndroidSchedulers.mainThread())
                 .toLiveData()
     }
 
     fun onRefresh() = refreshSubject.onNext(Unit)
 
+    fun onStartInvestigation() {
+        compositeDisposable.add(addInvestigationInteractor.get()
+                .invoke(query.toInvestigation())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe({ either ->
+                    either.fold(ifLeft = {
+                        savedStateHandle.set(KEY_START_INVESTIGATION_ERROR, it.localizedMessage())
+                    }, ifRight = {
+                        savedStateHandle.set(KEY_START_INVESTIGATION_SUCCESS, it.bundle(Investigation.serializer()))
+                    })
+                }, {
+                    Timber.error(it, it::toString)
+                    savedStateHandle.set(KEY_START_INVESTIGATION_ERROR, somethingWentWrong(it))
+                }))
+    }
+
+    fun onInvalidateAllQueries() {
+        compositeDisposable.add(invalidateAllQueriesInteractor.get()
+                .invoke()
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe({ }, {
+                    Timber.error(it, it::toString)
+                }))
+    }
+
+    override fun onCleared() {
+        compositeDisposable.dispose()
+        super.onCleared()
+    }
+
+    sealed class State {
+        data class Data(val items: Map<SimpleRetrievedChild, Weight>) : State()
+        object NoData : State()
+        object Loading : State()
+        object NoInternet : State()
+        object NoSignedInUser : State()
+        object Error : State()
+    }
+
     @Reusable
     class Factory @Inject constructor(
-            private val interactor: Provider<Lazy<FindChildrenInteractor>>,
-            private val filterFactory: Provider<Lazy<ChildrenFilterFactory>>
+            private val findChildrenInteractor: Provider<Lazy<FindChildrenInteractor>>,
+            private val addInvestigationInteractor: Provider<Lazy<AddInvestigationInteractor>>,
+            private val invalidateAllQueriesInteractor: Provider<Lazy<InvalidateAllQueriesInteractor>>
     ) : AssistedViewModelFactory<ChildrenSearchResultsViewModel> {
         override fun invoke(handle: SavedStateHandle): ChildrenSearchResultsViewModel {
-            return ChildrenSearchResultsViewModel(handle, interactor.get(), filterFactory.get())
+            return ChildrenSearchResultsViewModel(
+                    handle,
+                    findChildrenInteractor.get(),
+                    addInvestigationInteractor.get(),
+                    invalidateAllQueriesInteractor.get()
+            )
         }
     }
 
@@ -54,9 +163,14 @@ internal class ChildrenSearchResultsViewModel(
         private const val KEY_CHILD_QUERY =
                 "dev.ahmedmourad.sherlock.android.viewmodel.fragments.children.key.CHILD_QUERY"
 
-        fun defaultArgs(query: ChildQuery): Bundle? {
+        private const val KEY_START_INVESTIGATION_ERROR =
+                "dev.ahmedmourad.sherlock.android.viewmodel.fragments.children.key.ERROR_START_INVESTIGATION"
+        private const val KEY_START_INVESTIGATION_SUCCESS =
+                "dev.ahmedmourad.sherlock.android.viewmodel.fragments.children.key.ERROR_START_INVESTIGATION"
+
+        fun defaultArgs(query: ChildrenQuery): Bundle? {
             return Bundle(1).apply {
-                putBundle(KEY_CHILD_QUERY, query.bundle(ChildQuery.serializer()))
+                putBundle(KEY_CHILD_QUERY, query.bundle(ChildrenQuery.serializer()))
             }
         }
     }
