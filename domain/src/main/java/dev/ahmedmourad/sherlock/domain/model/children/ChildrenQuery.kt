89a973cfcf86b095da8a0a97661df3a37e87fc6a
Ahmed Mourad <dev.ahmedmourad73744@gmail.com>
2020-09-07 09:34:46 +0200

    Created ChildQuery and Investigation data classes

---
+++ domain/src/main/java/dev/ahmedmourad/sherlock/domain/model/children/ChildrenQuery.kt
@@ -0,0 +1,62 @@
+package dev.ahmedmourad.sherlock.domain.model.children
+
+import arrow.core.Either
+import arrow.core.left
+import arrow.core.right
+import dev.ahmedmourad.nocopy.annotations.NoCopy
+import dev.ahmedmourad.sherlock.domain.model.auth.SimpleRetrievedUser
+import dev.ahmedmourad.sherlock.domain.model.children.submodel.Coordinates
+import dev.ahmedmourad.sherlock.domain.model.children.submodel.ExactAppearance
+import dev.ahmedmourad.sherlock.domain.model.children.submodel.FullName
+import kotlinx.serialization.Serializable
+
+//TODO: add picture and lost date
+@Serializable
+@NoCopy
+data class ChildrenQuery private constructor(
+        val user: SimpleRetrievedUser,
+        val fullName: FullName,
+        val coordinates: Coordinates,
+        val appearance: ExactAppearance,
+        val page: Int
+) {
+
+    fun toInvestigation() = Investigation.of(
+            System.currentTimeMillis(),
+            user,
+            fullName,
+            coordinates,
+            appearance
+    )
+
+    companion object {
+
+        fun of(user: SimpleRetrievedUser,
+               fullName: FullName,
+               coordinates: Coordinates,
+               appearance: ExactAppearance,
+               page: Int
+        ): Either<Exception, ChildrenQuery> {
+            return validate(user, fullName, coordinates, appearance, page)?.left()
+                    ?: ChildrenQuery(user, fullName, coordinates, appearance, page).right()
+        }
+
+        @Suppress("UNUSED_PARAMETER")
+        fun validate(user: SimpleRetrievedUser,
+                     fullName: FullName,
+                     coordinates: Coordinates,
+                     appearance: ExactAppearance,
+                     page: Int
+        ): Exception? {
+            return if (page < 0) {
+                null
+            } else {
+                Exception.InvalidPageNumberException
+            }
+        }
+    }
+
+    sealed class Exception {
+        object InvalidPageNumberException : Exception()
+    }
+}
--- domain/src/main/java/dev/ahmedmourad/sherlock/domain/model/children/Investigation.kt
+++ domain/src/main/java/dev/ahmedmourad/sherlock/domain/model/children/Investigation.kt
@@ -2,27 +2,29 @@ package dev.ahmedmourad.sherlock.domain.model.children
 
 import dev.ahmedmourad.nocopy.annotations.NoCopy
 import dev.ahmedmourad.sherlock.domain.model.auth.SimpleRetrievedUser
+import dev.ahmedmourad.sherlock.domain.model.children.submodel.Coordinates
 import dev.ahmedmourad.sherlock.domain.model.children.submodel.ExactAppearance
 import dev.ahmedmourad.sherlock.domain.model.children.submodel.FullName
-import dev.ahmedmourad.sherlock.domain.model.children.submodel.Location
 import kotlinx.serialization.Serializable
 
 //TODO: add picture and lost date
 @Serializable
 @NoCopy
-data class ChildQuery private constructor(
+data class Investigation private constructor(
+        val timestamp: Long,
         val user: SimpleRetrievedUser,
         val fullName: FullName,
-        val location: Location,
+        val coordinates: Coordinates,
         val appearance: ExactAppearance
 ) {
     companion object {
-        fun of(user: SimpleRetrievedUser,
+        fun of(timestamp: Long,
+               user: SimpleRetrievedUser,
                fullName: FullName,
-               location: Location,
+               coordinates: Coordinates,
                appearance: ExactAppearance
-        ): ChildQuery {
-            return ChildQuery(user, fullName, location, appearance)
+        ): Investigation {
+            return Investigation(timestamp, user, fullName, coordinates, appearance)
         }
     }
 }
