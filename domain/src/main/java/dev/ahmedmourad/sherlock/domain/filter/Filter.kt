79d10e85acf296943c48c4668bd3fcf52883c0a1
Ahmed Mourad <dev.ahmedmourad73744@gmail.com>
2020-09-07 09:33:55 +0200

    Deleted the client-side Filter entirely

--- domain/src/main/java/dev/ahmedmourad/sherlock/domain/filter/Filter.kt
+++
@@ -1,9 +0,0 @@
-package dev.ahmedmourad.sherlock.domain.filter
-
-import dev.ahmedmourad.sherlock.domain.model.children.submodel.Weight
-
-//TODO: should be a function
-interface Filter<T> {
-    //TODO: return a double as a ratio that this is the right child
-    fun filter(items: List<T>): Map<T, Weight>
-}
