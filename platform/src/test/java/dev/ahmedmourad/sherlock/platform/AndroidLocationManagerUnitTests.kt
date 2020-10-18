7e2ad0c74c90dfb24614a20b423e2790a0340b1a
Ahmed Mourad <dev.ahmedmourad73744@gmail.com>
2020-10-18 18:45:24 +0200

    Added Platform module tests

--- platform/src/test/java/dev/ahmedmourad/sherlock/platform/AndroidLocationManagerUnitTests.kt
+++ platform/src/test/java/dev/ahmedmourad/sherlock/platform/AndroidLocationManagerUnitTests.kt
@@ -1,33 +1,24 @@
 package dev.ahmedmourad.sherlock.platform
 
-import androidx.test.ext.junit.runners.AndroidJUnit4
 import dev.ahmedmourad.sherlock.platform.managers.AndroidLocationManager
 import org.junit.Assert.assertEquals
 import org.junit.Before
 import org.junit.Test
-
 import org.junit.runner.RunWith
-import timber.log.Timber
-import timber.log.error
+import org.robolectric.RobolectricTestRunner
 
-@RunWith(AndroidJUnit4::class)
-class AndroidLocationManagerInstrumentedTests {
+@RunWith(RobolectricTestRunner::class)
+class AndroidLocationManagerUnitTests {
 
     private lateinit var manager: AndroidLocationManager
 
     @Before
     fun setup() {
-        Timber.plant(Timber.DebugTree())
-        setupManager()
-    }
-
-    private fun setupManager() {
         manager = AndroidLocationManager()
     }
 
     @Test
-    fun distanceBetween_shouldReturnTheDistanceInMetersBetweenTheTwoCoordinates() {
-
+    fun `distanceBetween should return the distance in meters between the two coordinates`() {
         mapOf(18504L to ((30.7895331 to 30.9902482) to (30.8254819 to 30.8014204)),
                 4076L to ((30.7895331 to 30.9902482) to (30.8261675 to 30.9938675)),
                 18412L to ((30.8254819 to 30.8014204) to (30.8261675 to 30.9938675))
@@ -38,7 +29,7 @@ class AndroidLocationManagerInstrumentedTests {
                     coordinates.second.second
             )
         }.forEach { (systemDistance, managerDistance) ->
-            Timber.error("$systemDistance  -  $managerDistance")
+            println("$systemDistance  -  $managerDistance")
             assertEquals(systemDistance, managerDistance)
         }
     }
