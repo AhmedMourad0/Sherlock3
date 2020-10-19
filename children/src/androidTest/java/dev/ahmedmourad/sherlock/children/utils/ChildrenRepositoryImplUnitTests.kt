49db607abf451d243a530e4182dc00a2054d2d02
Ahmed Mourad <dev.ahmedmourad73744@gmail.com>
2020-10-19 20:00:47 +0200

    Added Robolectric

---
+++ children/src/androidTest/java/dev/ahmedmourad/sherlock/children/utils/ChildrenRepositoryImplUnitTests.kt
@@ -0,0 +1,42 @@
+package dev.ahmedmourad.sherlock.children.utils
+
+import android.text.format.DateUtils
+import androidx.test.core.app.ApplicationProvider
+import dev.ahmedmourad.sherlock.children.repository.ChildrenRepositoryImpl
+import dev.ahmedmourad.sherlock.platform.managers.AndroidDateManager
+import org.junit.Assert.assertEquals
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.RobolectricTestRunner
+
+@RunWith(RobolectricTestRunner::class)
+class ChildrenRepositoryImplUnitTests {
+
+    private lateinit var repo: ChildrenRepositoryImpl
+
+    @Before
+    fun setup() {
+        repo = ChildrenRepositoryImpl()
+    }
+
+    @Test
+    fun `getRelativeDateTimeString should return the correct relative date time string`() {
+        listOf(System.currentTimeMillis() + 2000000,
+                System.currentTimeMillis() - 2000000,
+                System.currentTimeMillis() - 4000000,
+                System.currentTimeMillis() - 48000000,
+                System.currentTimeMillis() - 100000000,
+                System.currentTimeMillis() - 550000000,
+                System.currentTimeMillis() - 2000000000).map {
+            (DateUtils.getRelativeDateTimeString(ApplicationProvider.getApplicationContext(),
+                    it,
+                    DateUtils.MINUTE_IN_MILLIS,
+                    DateUtils.WEEK_IN_MILLIS,
+                    0).toString()
+                    to manager.getRelativeDateTimeString(it))
+        }.forEach { (systemValue, managerValue) ->
+            assertEquals(systemValue, managerValue)
+        }
+    }
+}
